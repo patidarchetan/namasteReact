35336e8463b84baa4eaa91577789453340f5077f
Chetan Patidar <chetan.patidar.2002@gmail.com>
2023-07-20 09:48:14 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # REACT
+
+# Parcel
+ - Dev Build
+ - Local Server
+ - HMR - Hot Module Replacement
+ - File Watching Algorithm - written in C++
+ - Caching - Faster builds
+ - Image Optimization
+ - Minification
+ - Bundling
+ - Compress
+ - Consistent hashing
+ - Code Splitting
+ - Differential Bundling -
+ - Diagnostic
+ - Error Handling
+ - HTTPs
+ - Tree Shaking - remove unused code
--- app.js
+++ app.js
@@ -1,33 +1,56 @@
-/*
-<div id="parent">
-        <div id="child">
-         <h1>I'm h1 tag</h1>
-        </div>
-</div>
-*/
-
-// const parent = React.createElement("div",
-//   { id: "parent" }, React.createElement("div",
-//     { id: "child" }, React.createElement("h1", {}, "I'm h1 tag")
-//   )
+import React from "react";
+import ReactDOM from "react-dom/client";
+////////////////////Lecture3///////////////////////////////
+// //React Element
+// const jsxHeading = (
+//   <h1 className="head">
+//     Hello world
+//   </h1>
 // );
-//                 If want to create siblings..........//
-/*
-<div id="parent">
-        <div id="child">
-         <h1>I'm h1 tag</h1>
-         <h2>I'm h2 tag</h2>
-        </div>
-</div>
-*/
-const parent = React.createElement("div",
-   { id: "parent" }, React.createElement("div",
-     { id: "child" },[
-       React.createElement("h1", {}, "I'm h1 tag"),
-       React.createElement("h2", {}, "I'm h2 tag")
-     ]
-   )
- );
-// const heading = React.createElement("h1", { id: "heading" }, "Hello from react");
+
+// const elem = <span>Hello</span>
+// const Title = () => (
+//   <div>
+//   {elem}
+//     <h1>Chetan here</h1>
+//   </div>
+// )
+
+// const Title2 = () => (
+//   <div>
+//   <Title />
+//     <h1>Patidar</h1>;
+//   </div>
+// )
+
+// //React Component
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+// root.render(<Title2/>)
+
+const header = React.createElement("div", { class: "title" }, [
+  React.createElement("h1", {}, "H1"),
+  React.createElement("h2", {}, "H2"),
+  React.createElement("h3", {}, "H3"),
+]);
+
+const headerJsx = (
+  <div id="class">
+    <h1>"H1"</h1>
+    <h2>"H2</h2>
+    <h3>"H3</h3>
+  </div>
+)
+
+
+const HeaderJsx = () => (
+  <div className = "title">
+    <h1>"H1"</h1>
+    <h2>"h2"</h2>
+    <h3>"h3"</h3>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeaderJsx />);
+
+//////////////////////////////lecture 3/////////////////////////////
